249949773a85dadeab484bc20fac13f77c15f636
abhayluv <abhayl.gc@gmail.com>
2025-08-29 19:03:57 +0530

    dashboard analytics set

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,13 +12,21 @@
     <div class="flex-1 p-4 overflow-y-auto">
         <ul class="space-y-1">
             <li>
-                <x-sidebar-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="w-full">
+                <x-sidebar-link :href="route('analytics.index')" :active="request()->routeIs('analytics.*')" class="w-full">
                     <svg class="mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                     </svg>
                     <span>{{ __('Dashboard') }}</span>
                 </x-sidebar-link>
             </li>
+            <!--<li>
+                <x-sidebar-link :href="route('analytics.index')" :active="request()->routeIs('analytics.*')" class="w-full">
+                    <svg class="mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 13a4 4 0 017.48 1v7A4 4 0 019 25a4 4 0 01-4-4v-7a4 4 0 014-4zM21 5a4 4 0 00-7.48-1v7a4 4 0 004 4 4 4 0 004-4V5z" />
+                    </svg>
+                    <span>{{ __('Dashboard') }}</span>
+                </x-sidebar-link>
+            </li>-->
             <li x-data="{ open: {{ request()->routeIs('configurations.*') || request()->routeIs('roles.*') || request()->routeIs('users.*') ? 'true' : 'false' }} }">
                 <x-sidebar-dropdown 
                     :active="request()->routeIs('configurations.*') || request()->routeIs('roles.*') || request()->routeIs('users.*')" 
@@ -63,14 +71,6 @@
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                     </svg>
                     <span>{{ __('Master Form') }}</span>
-                </x-sidebar-link>
-            </li>
-            <li>
-                <x-sidebar-link :href="route('analytics.index')" :active="request()->routeIs('analytics.*')" class="w-full">
-                    <svg class="mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 13a4 4 0 017.48 1v7A4 4 0 019 25a4 4 0 01-4-4v-7a4 4 0 014-4zM21 5a4 4 0 00-7.48-1v7a4 4 0 004 4 4 4 0 004-4V5z" />
-                    </svg>
-                    <span>{{ __('Analytics') }}</span>
                 </x-sidebar-link>
             </li>
             <li>
